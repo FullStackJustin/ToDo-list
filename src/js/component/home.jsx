5be4e9e0573d7b17ccf7b486e64ed37d2be42254
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-04-16 02:08:32 +0000

    again

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,38 @@
-import React from "react";
-
+import React, { component } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	constructor(props){
+		super(props);
+		this.state = {
+			items: []
+		}
+		this.addItem = this.addItem.bind(this)
+	}
+	addItem(e) {
+		if (this._inputElement.value !== ""){
+			var newItem = {
+				text: this._inputElement.value,
+				key: Date.now()
+			}
+			this.setState((prevState) =>{
+					return {
+						items: prevState.items.concat(newItem)
+				};
+				});
+		};
+		this._inputElement.value = "";
+		console.log(this.state.value);
+		e.preventdefault();
+	}
 	return (
-		<div>
-			<div>
-				<form>
+		<div className="todoListMain">
+			<div className="header">
+				<form onSubmit={this.addItem}>
+					<input ref={(a) => this._inputElement = a} placeholder="I Need To..."></input>
+					<button type="button">add</button>
 				</form>
 			</div>
 		</div>
