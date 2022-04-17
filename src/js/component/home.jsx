a25f565ae52bc9c5d8795e78c77ab8353b349dec
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-04-17 22:54:06 +0000

    update on todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,40 +3,50 @@ import React, { component } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	constructor(props){
-		super(props);
-		this.state = {
-			items: []
-		}
-		this.addItem = this.addItem.bind(this)
-	}
-	addItem(e) {
-		if (this._inputElement.value !== ""){
-			var newItem = {
-				text: this._inputElement.value,
-				key: Date.now()
-			}
-			this.setState((prevState) =>{
-					return {
-						items: prevState.items.concat(newItem)
-				};
-				});
-		};
-		this._inputElement.value = "";
-		console.log(this.state.value);
-		e.preventdefault();
-	}
+function Home(props) {
 	return (
-		<div className="todoListMain">
-			<div className="header">
-				<form onSubmit={this.addItem}>
-					<input ref={(a) => this._inputElement = a} placeholder="I Need To..."></input>
-					<button type="button">add</button>
-				</form>
+		<div>
+			<h1>To-Do List</h1>
+			<form>
+				<input
+					type="text"
+					id="new-todo-input"
+					className="input input__lg"
+					name="text"
+					autoComplete="off"
+				/>
+				<button type="submit" className="btn btn__primary btn__lg">
+					Add
+				</button>
+			</form>
+			<div className="filters btn-group stack-exception">
+				<button
+					type="button"
+					className="btn toggle-btn"
+					aria-pressed="true">
+					<span className="visually-hidden">Show </span>
+					<span>all</span>
+					<span className="visually-hidden"> tasks</span>
+				</button>
+				<button
+					type="button"
+					className="btn toggle-btn"
+					aria-pressed="false">
+					<span className="visually-hidden">Show </span>
+					<span>Active</span>
+					<span className="visually-hidden"> tasks</span>
+				</button>
+				<button
+					type="button"
+					className="btn toggle-btn"
+					aria-pressed="false">
+					<span className="visually-hidden">Show </span>
+					<span>Completed</span>
+					<span className="visually-hidden"> tasks</span>
+				</button>
 			</div>
 		</div>
 	);
-};
+}
 
 export default Home;
